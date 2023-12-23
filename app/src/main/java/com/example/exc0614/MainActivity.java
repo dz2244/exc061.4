b7a13d0d3e46d34d216d57ccb86e6a26b94470a3
dz2244 <dz2244@bs.amalnet.k12.il>
2023-12-23 20:24:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/exc0614/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.exc0614;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    TextView text3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        text3 = findViewById(R.id.text3);
+    }
+    int counter = 0 ;
+    public void ace(View view) {
+
+
+    }
+}
